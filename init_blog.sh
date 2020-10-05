018e1adacc03815cbf42680a303e345d151462b7
WangCC <gogkatsu@outlook.com>
2020-10-05 08:38:56 +0800

    Update init_blog.sh

--- init_blog.sh
+++ init_blog.sh
@@ -1,1 +1,8 @@
 # 初始化博客环境
+
+sudo apt install -y ruby-dev  
+sudo gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/
+sudo gem install bundler jekyll
+cd;git clone git@github.com:JackLovel/JackLovel.github.io.git
+cd JackLovel.github.io
+bundle install 
